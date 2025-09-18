744e052a2e3c9c8ef84d2e1baf03b02250f54417
Jotape777bjs <joaopedrosantanadebarros4@gmail.com>
2025-09-18 18:54:37 +0000

    adicionei os objetivos do tema 4 nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -1,46 +1,74 @@
 #include <stdio.h>
 
+void recusivaLoop(int rainha){//função para o movimento da rainha
+        if (rainha <= 8){
+            printf("Rainha se movendo %d casas a esquerda\n", rainha);
+            recusivaLoop(rainha + 1 );
+        }
+}
 
+void recusivaloop2(int torre){
+        if (torre <= 5){
+            printf("Torre se movendo %d casas a direita\n", torre);
+            recusivaloop2(torre + 1);
+        }
+}
+
+void recusivaloop3(int bispo_direita){
+        if(bispo_direita <= 5){
+            printf("Bispo se movendo %d casas para cima\n", bispo_direita);
+            recusivaloop3(bispo_direita + 1);
+        }
+}
+    
 int main() {
     
     int movimento_rainha = 1, movimento_bispo = 1;//variaveis declaradas a varivale da torre não esta aqui por que usei ela no for
-    int movimento_cavalo = 0;//variavel para a movimentação do cavalo
+    int movimento_cavalo = 1, movimento_torre = 1;//variavel para a movimentação do cavalo
 
-
-    while (movimento_rainha <= 8)//esse bloco tem a função de realizar os movimentos da rainha
-    {
-        printf("Rainha se movendo %d casas a esquerda\n", movimento_rainha);
-        movimento_rainha++;
-    }
+        recusivaLoop(movimento_rainha);
     
     printf("\n");
 
-    do//esse bloco faz o movimento do bispo
-    {
-        printf("Bispo se movendo %d casas a cima\n", movimento_bispo);
-        printf("Bispo se movendo %d casas para direita\n", movimento_bispo);
-        movimento_bispo++;
-    } while (movimento_bispo <= 5);
+    while (movimento_bispo <= 5){
+
+        for (int movi_bispo = 1; movi_bispo <= 5; movi_bispo++)
+        {
+            printf("Bispo se movendo %d casas a direita\n", movi_bispo);
+        }
+        recusivaloop3(movimento_bispo);
+        break;
+    }
        
     printf("\n");//para deixar legivel no terminal
 
-    for(int movimento_torre = 1; movimento_torre <= 5; movimento_torre++){//faz o movimento da torre e por isso não declarei a variavel dela anteriomente por que ele tem que estar dentro do for para funcionar
-        printf("Torre se movendo %d casas a direita\n", movimento_torre);
-    }
+    recusivaloop2(movimento_torre);
     
     printf("\n");
 
-    while (movimento_cavalo < 1)//repetição externa
-    {
-        for (int movi_cava = 1; movi_cava <= 2; movi_cava++)//retição interna
-        {
-            printf("Cavalo se movendo %d casas para baixo\n", movi_cava);
+   
+    for (int movi_cava_cima = 2; movi_cava_cima <= 2;movi_cava_cima++)//for para utilizar estrutura avançadas de loop
+    {   
+        if (movi_cava_cima){
+            printf("Cavalos se movendo %d casas para cima\n", movi_cava_cima);
+            
+        }else if (movi_cava_cima > 2){
+                for (int movi_cava_direita = 1; movi_cava_direita <= 1;movi_cava_direita++){
+                    printf("Cavalos se movendo %d casas para a direita\n", movi_cava_direita);
+                }
         }
         
-        printf("Cavalo se movendo %d casas para a esquerda\n", movimento_cavalo);
-        movimento_cavalo++;//incremento para que a condição da repetição externa se torne falsa
+        
+            for (int movi_cava_direita = 1; movi_cava_direita <= 1;movi_cava_direita++)
+            {
+                printf("Cavalos se movendo %d casa para a direita\n", movi_cava_direita);
+            }
+        
+        
+        
     }
-    
+        
+      
     printf("Encerramos nossas movimentações no jogo de hoje!\n");
     
 
